ca02483a0fdb33118c310ffe4d000ad27910b94a
Aman Anand <69569434+PulsatingGenius@users.noreply.github.com>
2020-08-20 10:40:04 +0530

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,22 @@
+import random
+import string
+sep = ""
+random.choice(string.ascii_letters)
+password = []
+let1 = random.randint(1,9)
+let2 = random.randint(1,9)
+let3 = random.choice(string.ascii_letters)
+let4 = random.randint(1,9)
+let5 = random.choice(string.ascii_letters)
+let6 = random.choice(string.ascii_letters)
+let7 = random.randint(1,9)
+let8 = random.choice(string.ascii_letters)
+let9 = random.randint(1,9)
+let10 = random.randint(1,9)
+let11 = random.choice(string.ascii_letters)
+let12 = random.randint(1,9)
+let13 = random.choice(string.ascii_letters)
+let14 = random.choice(string.ascii_letters)
+let15 = random.randint(1,9)
+let16 = random.choice(string.ascii_letters)
+print(let1,let2,let3,let4,let5,let6,let7,let8,let9,let10,let11,let12,let13,let14,let15,let16, sep="")
